ab8f15910e840df16e0dd7086d2e111eb1632e9c
profundus59 <profundus59@gmail.com>
2026-01-23 16:46:11 +0900

    NEW FILE: added bash script to download pretrained weights

---
+++ pretrained_weights/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gdown --id 1zFBTmWhHQn_QxN84hpT-fA0C5TzZUAZK
+
+gdown --id 1FKIdXM9UEHSn93Xhbqi2hYBktDRMBsLr
+
+gdown --id 1pM7mBgvHtI_XNbfkX5eKT4jsUaLL3Qa4
+
+gdown --id 1gbyknvuOiKpxK1kljxFnt1k5Jxp3R1jv
